65e373d5a1e2e80ea0f6f4474888643c5a203102
Gabriel Arriola <gabriel.arriola.cdia@gmail.com>
2022-11-21 00:19:48 -0300

    Se agregan graficas y templates

--- CDIA_H1006/app.py
+++ CDIA_H1006/app.py
@@ -1,26 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
-from consultas import create_tables, insert_marca, consulta_marca, insert_auto
+from consultas import *
 from lista_modelos import get_urls
 import requests
 import sqlite3
+import matplotlib.pyplot as plt
+import base64
+import io
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    return render_template('home.html')
 
 
-@app.route('/createdatabase', methods=['POST'])
+@app.route('/createdatabase', methods=['GET'])
 def create_db():  # put application's code here
     con = sqlite3.connect("./info/autocosmos.db")
     cursor = con.cursor()
     return create_tables(cursor)
 
 
-@app.route('/populatedatabase', methods=['POST'])
+@app.route('/populatedatabase', methods=['GET'])
 def populate_db():  # put application's code here
     con = sqlite3.connect("./info/autocosmos.db")
     cur = con.cursor()
@@ -49,15 +52,15 @@ def populate_db():  # put application's code here
                 nombre = modelo.find('span', class_='model-card__brand').text
                 moneda = modelo.find('span', class_='model-card__price-value')
                 if moneda is None:
-                    moneda = '--'
+                    moneda = 'S/D'
                     precio = '0'
                 else:
                     if 'u$s' in moneda.text[0:3]:
                         moneda = moneda.text[0:3]
                         precio = modelo.find('span', class_='model-card__price-value').text.replace('.', '')[3:]
                     else:
-                        moneda = moneda.text[0:1]
-                        precio = modelo.find('span', class_='model-card__price-value').text.replace('.', '')[1:]
+                        moneda = 'u$s'  # moneda.text[0:1]
+                        precio = str(round(int(modelo.find('span', class_='model-card__price-value').text.replace('.', '')[1:])/300, 2))  # conversion a dolar con una cotizacion blue de $300
                 tipo = modelo.find('span', class_='model-card__ribbon').text
                 origen = modelo.find('span', class_='model-card__origin').find('span').text
                 imagen = modelo.find('img')['src']
@@ -70,6 +73,87 @@ def populate_db():  # put application's code here
 
     return 'Tablas cargadas'
 
+# img = io.BytesIO()
+
+@app.route('/marcas')
+def read_marcas():
+    con = sqlite3.connect("./info/autocosmos.db")
+    cursor = con.cursor()
+    df = total_por_marca(con)
+    con.close()
+
+    img = io.BytesIO()
+    # grafico de barras
+    plt.clf()
+    plt.title("Total de vehiculos catalogados por fabricante", fontsize=25, fontweight='bold', color='blue', loc='center', style='italic')
+    lista = df.values.tolist()
+    for i in range(len(lista)):
+        plt.barh(lista[i][0], lista[i][1])
+    plt.gcf().set_size_inches(12, 10)
+    plt.savefig(img, format='png', dpi=90)
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('grafica.html', imagen={'imagen': plot_url})
+
+@app.route('/categorias')
+def read_categorias():
+    con = sqlite3.connect("./info/autocosmos.db")
+    cursor = con.cursor()
+    df = total_por_categoria(con)
+    con.close()
+
+    img = io.BytesIO()
+    # grafico de torta
+    plt.clf()
+    plt.title("Distribución de vehiculos por segmentos", fontsize=25, fontweight='bold', color='blue', loc='center', style='italic')
+    plt.pie(df['cantidad'].tolist(), labels=df['tipo'].tolist(), autopct='%1.1f%%')
+    plt.gcf().set_size_inches(12, 10)
+    plt.savefig(img, format='png', dpi=80)
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('grafica.html', imagen={'imagen': plot_url})
+
+
+@app.route('/categorias/<modelo>')
+def read_categorias_modelo(modelo):
+    con = sqlite3.connect("./info/autocosmos.db")
+    cursor = con.cursor()
+    df = precio_por_categoria(con, str(modelo))
+    con.close()
+
+    img = io.BytesIO()
+    # grafico de puntos
+    plt.clf()
+    plt.title("Precios de modelos para el segmento " + str(modelo) +" (U$s)", fontsize=25, fontweight='bold', color='blue', loc='center', style='italic')
+    plt.scatter(df['precio'], df['marca'], color='brown')
+    plt.tick_params(axis='x', rotation=45)
+    plt.gcf().set_size_inches(20, 10)
+    plt.savefig(img, format='png', dpi=80)
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('grafica.html', imagen={'imagen': plot_url})
+
+
+@app.route('/marcas/<marca>')
+def read_precio_marca(marca):
+    con = sqlite3.connect("./info/autocosmos.db")
+    cursor = con.cursor()
+    df = precio_por_marca(con, str(marca))
+    con.close()
+
+    img = io.BytesIO()
+    # grafico de puntos
+    plt.clf()
+    plt.title("Precios de modelos para el fabricante " + str(marca) +" (U$s)", fontsize=25, fontweight='bold', color='blue', loc='center', style='italic')
+    plt.plot(df['precio'], df['modelo'], color='r')
+    plt.tick_params(axis='x', rotation=45)
+    plt.gcf().set_size_inches(20, 10)
+    plt.savefig(img, format='png', dpi=80)
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('grafica.html', imagen={'imagen': plot_url})
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
--- CDIA_H1006/consultas.py
+++ CDIA_H1006/consultas.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 def create_tables(cursor):
     cursor.execute("""CREATE TABLE IF NOT EXISTS marcas (
@@ -37,4 +37,39 @@ def consulta_marca(cursor, marca):
 def insert_auto(cursor, datos):
     cursor.executemany('insert into autos (marca_id, modelo, moneda, precio, tipo, sub_tipo, origen, url_img) values (?, ?, ?, ?, ?, ?, ?, ?)', [info for info in datos])
     cursor.connection.commit()
+
+
+def total_por_marca(con):
+    df = pd.read_sql_query('''select m.nombre as marca, count(*) as cantidad
+                                from autos a 
+                                join marcas m 
+                                on a.marca_id =m.id 
+                                group by m.nombre 
+                                ORDER by 1;''', con)
+    return df
+
+
+def total_por_categoria(con):
+    df = pd.read_sql_query('''select a.tipo, count(*) as cantidad
+                                from autos a 
+                                where a.precio > 0
+                                group by  a.tipo
+                                ORDER by 1;''', con)
+    return df
+
+
+def precio_por_categoria(con, modelo):
+    df = pd.read_sql_query('''select m.nombre || ": " || a.modelo as marca, a.precio as precio, a.tipo
+                                from autos a join marcas m on a.marca_id =m.id 
+                                and a.precio > 0 and a.tipo = "''' + modelo + '''"
+                                group by a.tipo,a.moneda ,m.nombre order by 2;''', con)
+    return df
+
+
+def precio_por_marca(con, marca):
+    df = pd.read_sql_query('''select a.modelo, a.precio as precio
+                                from autos a join marcas m on a.marca_id =m.id 
+                                and a.precio > 0 and m.nombre = "''' + marca + '''"
+                                ORDER by 2;''', con)
+    return df
 
